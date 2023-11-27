b4a2169796032c5349cda60ccdb010cf7015ea36
Ben Gruey <91962409+bgruey@users.noreply.github.com>
2023-11-26 21:07:48 -0600

    debuggery

--- libs/pinio/pwm_out.c
+++ libs/pinio/pwm_out.c
@@ -2,15 +2,17 @@
 #include "pinio.h"
 
 
-void run_pwm_on_pin(PWMData_t* pwm_data, int* run_bool, uint8_t pin) {
+void run_inverted_pwm_on_pin(PWMData_t* pwm_data, int* run_bool, uint8_t pin) {
+    double sleep_high = pwm_data->sleep_high;
 
     while (run_bool[0]) {
-        write_pin_high(pin);
-        sleep_via_double(pwm_data->sleep_high, &pwm_data->sleep_data.now);
         write_pin_low(pin);
+        sleep_via_double(sleep_high, &pwm_data->sleep_data.now);
+        write_pin_high(pin);
         sleep_via_double(
-            pwm_data->period_seconds - pwm_data->sleep_high,
+            pwm_data->period_seconds - sleep_high,
             &pwm_data->sleep_data.now
         );
+        sleep_high = pwm_data->sleep_high;
     }
 }
