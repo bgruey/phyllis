ae03857b5a816fe4749ef4fb1377dcf2efa1fb9a
Ben Gruey <91962409+bgruey@users.noreply.github.com>
2023-11-29 19:54:52 -0600

    where is pin gpio 3???

--- libs/pinio/pwm_out.c
+++ libs/pinio/pwm_out.c
@@ -5,7 +5,7 @@
 void run_inverted_pwm_on_pin(PWMData_t* pwm_data, int* run_bool, uint8_t pin) {
     double sleep_high = pwm_data->sleep_high;
     fprintf(stderr, "Starting PWM with period %f\n", pwm_data->period_seconds);
-    while (run_bool) {
+    while (run_bool[0]) {
         // fprintf(stderr, "PWM for %f\n", sleep_high);
         write_pin_low(pin);
         sleep_via_double(sleep_high, &pwm_data->sleep_data.now);
@@ -19,6 +19,7 @@ void run_inverted_pwm_on_pin(PWMData_t* pwm_data, int* run_bool, uint8_t pin) {
 
         sleep_high = pwm_data->sleep_high;
     }
+    fprintf(stderr, "inverted wpm run_bool off.\n");
 }
 
 
